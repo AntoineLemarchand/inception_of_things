c66c43be4139e3a55037fda93c48615c9524bfa9
user42 <user42@iot>
2023-07-26 09:09:48 -0400

    feat(p3): done

--- p3/scripts/deploy.sh
+++ p3/scripts/deploy.sh
@@ -1,15 +1,23 @@
 # argocd deploy: https://www.youtube.com/watch?v=2oWtBDOJgUM
 
-sudo k3d cluster create argocd 
+sudo k3d cluster create argocd -p "8888:8888@loadbalancer"
 sudo kubectl config use-context k3d-argocd
-sudo kubectl create namespace argocd
-sudo kubectl config use-context k3d-argocd
+sudo kubectl create namespace argocd &&
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-while [[ $(sudo kubectl get pods -n argocd | grep Running | wc -l) != '7' ]]
+while [[ $(sudo kubectl get pods -n argocd 2> /dev/null | grep Running | wc -l) != '7' ]]
 do
-	echo "WAITING FOR ARGOCD... ($(sudo kubectl get pods -n argocd | grep Running | wc -l) / 7)" && sleep 10
+	echo "WAITING FOR ARGOCD... ($(sudo kubectl get pods -n argocd 2> /dev/null | grep Running | wc -l) / 7)" && sleep 10
 done
 
-echo "PASSWORD --> $(sudo kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)" &&
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+sudo kubectl create namespace dev &&
+sudo kubectl apply -f $(dirname "$0")/../confs/app.yaml
+
+while [[ $(sudo kubectl get pods -n dev 2> /dev/null 2> /dev/null | grep Running | wc -l) != '1' ]]
+do
+	echo "WAITING FOR APP... ($(sudo kubectl get pods -n dev 2> /dev/null | grep Running | wc -l) / 1)" && sleep 10
+done
+
+echo "LOGIN --> admin" &&
+echo "PASSWORD --> $(sudo kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)"
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null &
